3d2156e4a484472d999bbea298ff4104fdbd4283
dan9b6 <dkburton0@gmail.com>
2020-03-13 11:42:05 +0000

    Navbar Component Built

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+import NavBar from './Components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      Hello World
+    </div>
+  );
+}
+
+export default App;
