81d380167f449ff0f9f22d1fde19fe054b67c492
dan9b6 <dkburton0@gmail.com>
2020-03-13 15:41:43 +0000

    API leagues added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './views/home';
 import Leagues from './views/leagues';
-import NavBar from './Components/Navbar';
 import Login from './views/Login';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import signup from './views/Signup';
+import Club from './views/Club';
+
+import NavBar from './Components/Navbar';
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           <Route path="/leagues" component={Leagues} />
           <Route path="/login" component={Login} exact />
           <Route path="/sign-up" component={signup} exact />
+          <Route path="/league/club" component={Club} exact />
         </Switch>
       </BrowserRouter>
       <footer>this is going to be the footer</footer>
---
+++ client/src/views/Club/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SingleClub from '../../Components/SingleClub';
+
+class Club extends Component {
+  render() {
+    return (
+      <div>
+        <SingleClub />
+      </div>
+    );
+  }
+}
+
+export default Club;
