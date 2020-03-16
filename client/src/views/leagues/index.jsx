5c4698404fb9141f31134bdb88caf96705c88eb3
FilipeAVNunes <filipenunes407@hotmail.com>
2020-03-16 09:42:25 +0000

    leagues links

--- client/src/views/leagues/index.jsx
+++ client/src/views/leagues/index.jsx
@@ -36,7 +36,9 @@ export default class Leagues extends Component {
               return league.strSport === 'Soccer';
             })
             .map(league => (
-              <li key={league.idLeague}>{league.strLeague}</li>
+              <li key={league.idLeague}>
+                <Link>{league.strLeague}</Link>
+              </li>
             ))}
         </ul>
         {/* <ul>
