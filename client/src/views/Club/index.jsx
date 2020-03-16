f270164e82511fe310e688871507eb63072b008f
FilipeAVNunes <filipenunes407@hotmail.com>
2020-03-16 17:11:23 +0000

    club view done

--- client/src/views/Club/index.jsx
+++ client/src/views/Club/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { getTeamInfo } from '../../services/api-services';
+import { Link } from 'react-router-dom';
+import './style.scss';
 
 export default class ClubInfo extends Component {
   constructor(props) {
@@ -26,7 +28,35 @@ export default class ClubInfo extends Component {
       <div>
         {this.state.club &&
           this.state.club.map(val => {
-            return <h1>{val.strTeam}</h1>;
+            return (
+              <div>
+                <img src={val.strTeamBanner} alt={val.strTeam} />
+                <h1>{val.strTeam}</h1>
+                <p> {val.strAlternate} </p>
+                <p> {val.intFormedYear} </p>
+                <p>
+                  {val.strLeague}, {val.strCountry}
+                </p>
+                <img src={val.strTeamBadge} alt={val.strTeam} />
+                <img src={val.strTeamJersey} alt={val.strTeam} />
+                <p>{val.strDescriptionEN}</p>
+                <p>
+                  {val.strStadium} - {val.strStadiumLocation}
+                </p>
+                <p>Capacity: {val.intStadiumCapacity}</p>
+                <img src={val.strStadiumThumb} alt={val.strStadium} />
+                <article>
+                  <i>{val.strStadiumDescription}</i>
+                </article>
+                <div>
+                  <Link to={val.strWebsite}>Website</Link>
+                  <Link to={val.strFacebook}>Facebook</Link>
+                  <Link to={val.strTwitter}>Twitter</Link>
+                  <Link to={val.strInstagram}>Instagram</Link>
+                  <p></p>
+                </div>
+              </div>
+            );
           })}
       </div>
     );
